f09aa92b01a88205b9882af4a023b9e76a078e8f
senor <mikasaackerman280105@gmail.com>
2026-03-28 23:06:13 +0700

    xml2

---
+++ sitemap.js
@@ -0,0 +1,17 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://pajajaranjaya.vercel.app/",
+      lastModified: new Date(),
+      priority: 1.0,
+    },
+    {
+      url: "https://pajajaranjaya.vercel.app/kontak",
+      priority: 0.8,
+    },
+    {
+      url: "https://pajajaranjaya.vercel.app/layanan",
+      priority: 0.8,
+    },
+  ];
+}
